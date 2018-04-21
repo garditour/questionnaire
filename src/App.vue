be3f1725f0594b5e33f63b5c075fb64a32d53162
garditour <garditourcenter@gmail.com>
2018-04-21 12:41:56 +0700

    updated 21 April 2018

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <b-container>
+      <header class="my-5 pb-4 border-bottom d-flex justify-content-between">
+        <h1><i class="material-icons" style="font-size:36pt">assessment</i> Questionnaire</h1>
+        <b-nav pills class="d-flex justify-content-center"> 
+          <b-nav-item active><i class="material-icons">add</i> Add Questionnaire</b-nav-item>
+          <b-nav-item><i class="material-icons">multiline_chart</i> Show Report</b-nav-item>
+        </b-nav>
+      </header>
+      <router-view></router-view>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.nav-item a {
+  border-radius: 50px !important;
+}
+.material-icons{
+    display: inline-flex;
+    vertical-align: top;
+}
+</style>
---
+++ src/components/Add.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-row>
+      <b-col>
+        <b-form-group>
+          <label>Tour Name</label>
+          <b-input v-model="tour.name"/>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group>
+          <label>Tour Code</label>
+          <b-input v-model="tour.code"/>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group>
+          <label>Date</label>
+          <b-input type="date" v-model="tour.date"/>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-form-group>
+          <label>Name of Participant</label>
+          <b-input v-model="participant.name"/>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group>
+          <label>Phone</label>
+          <b-input v-model="participant.phone"/>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group>
+          <label>Email</label>
+          <b-input type="email" v-model="participant.email"/>
+        </b-form-group>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Add',
+  data () {
+    return {
+      tour: {
+        name: null,
+        code: null,
+        date: new Date().toISOString().split('T')[0]
+      },
+      participant: {
+        name: null,
+        phone: null,
+        email: null
+      }
+    }
+  }
+}
+</script>
